91460da4146a28a311564cf9398b0e66a6d958b7
Subramanian <subramanian.nk@live.com>
2021-06-02 17:55:28 +0530

    c

---
+++ Learning1/Learning1/Indexer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning1
+{
+    class Indexer
+    {
+
+        public void indexermain()
+        {
+            EmployeeI xc = new EmployeeI();
+            xc[0] = "a";
+            xc[1] = "b";
+            xc[2] = "c";
+
+            Console.WriteLine(xc[0] + xc[1] + xc[2]);
+
+        }
+
+    }
+
+    class EmployeeI
+    {
+        private string[] data = new string[5];
+
+        public string this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+
+            set
+            {
+                data[index] = value;
+            }
+        }
+    }
+}
